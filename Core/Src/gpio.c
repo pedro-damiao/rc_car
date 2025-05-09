77c81e18aa30a5f5d6f81ec48c2dc66a28b818fd
Cool69 <pedrodamiaosilva96@gmail.com>
2025-05-09 13:26:17 +0100

    [ADD] gpio for nss spi

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -52,6 +52,9 @@ void MX_GPIO_Init(void)
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
   /**/
+  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0);
+
+  /**/
   LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_4|LD2_Pin);
 
   /**/
@@ -72,6 +75,14 @@ void MX_GPIO_Init(void)
 
   /**/
   LL_GPIO_SetPinMode(B1_GPIO_Port, B1_Pin, LL_GPIO_MODE_INPUT);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /**/
   GPIO_InitStruct.Pin = LL_GPIO_PIN_4|LD2_Pin;
